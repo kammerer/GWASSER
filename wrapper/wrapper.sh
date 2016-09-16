b9dfd9b73fadcbc0f39e7103f5236decbe5d2727
Alice Minotto <i.m.e@hotmail.it>
2016-09-16 14:53:34 +0100

    Update wrapper.sh

--- wrapper/wrapper.sh
+++ wrapper/wrapper.sh
@@ -1,21 +1,21 @@
-GFILE=(${gFile})
-GFILE=${GFILE[@]:1}
-PFILE=(${pFile})
-PFILE=${PFILE[@]:1}
-PFILE=`for el in ${PFILE}; do echo ${el}", "; done`
-MFILE=(${mFile})
-MFILE=${MFILE[@]:1}
+#!/bin/bash
 
-ARGS="${gFile} ${pFile} ${mFile} ${PHENOS} ${COVS} --outDir output ${mkplots} ${saveinter}"
-INPUTS="${GFILE}", "${PFILE} ${MFILE}"
-echo ${INPUTS}
+GFILE=(${gFile})
+PFILE=`echo ${PFILE} | sed -e 's/ /, /g'`
+PFILE=(${PFILE})
+MFILE=(${mFile})
+INPUTS="${GFILE[@]:1}, ${PFILE[@]:1}, ${MFILE[@]:1}"
+CMDLINEARG="${gFile} ${pFile} ${mFile} ${PHENOS} ${COVS} --outDir output ${mkplots} ${saveinter}"
+echo inputs are ${INPUTS}
+echo arguments are ${CMDLINEARG}
+
 
 echo  universe                = docker >> lib/condorSubmitEdit.htc
 echo docker_image            =  cyverseuk/gwasser:v1.0.0 >> lib/condorSubmitEdit.htc ######
-echo arguments                          = ${ARGS} >> lib/condorSubmitEdit.htc
-echo transfer_input_files = ${INPUTS} launch.sh >> lib/condorSubmitEdit.htc
+echo arguments                          = ${CMDLINEARG} >> lib/condorSubmitEdit.htc
+echo transfer_input_files = ${INPUTS} >> lib/condorSubmitEdit.htc
 echo transfer_output_files = output >> lib/condorSubmitEdit.htc
-cat lib/condorSubmit.htc >> lib/condorSubmitEdit.htc
+cat /mnt/data/rosysnake/lib/condorSubmit.htc >> lib/condorSubmitEdit.htc
 
 less lib/condorSubmitEdit.htc
 
